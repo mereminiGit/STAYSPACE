f8f278941f8858296396ca76badc82ed6ea972b8
g2161290 <141694702+g2161290@users.noreply.github.com>
2023-09-25 09:05:53 +0900

    cummit

--- teamproject/src/main/java/co/yedam/teamproject/space/DetailController.java
+++ teamproject/src/main/java/co/yedam/teamproject/space/DetailController.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import co.yedam.teamproject.common.GeoCoder;
 import co.yedam.teamproject.common.ViewResolve;
 import co.yedam.teamproject.space.service.SpaceService;
 import co.yedam.teamproject.space.service.SpaceVO;
@@ -31,10 +30,10 @@ public class DetailController extends HttpServlet {
 		
 		vo.setSpaceId(Integer.parseInt(request.getParameter("spaceId")));
 		vo=dao.spaceSelect(vo);
-		String loaction=vo.getSpaceAddress();
-		Float[] coords = GeoCoder.geoCoding(loaction);
-		vo.setSpaceLat(coords[0]);
-		vo.setSpaceLng(coords[1]);
+//		String loaction=vo.getSpaceAddress();
+//		Float[] coords = GeoCoder.geoCoding(loaction);
+//		vo.setSpaceLat(coords[0]);
+//		vo.setSpaceLng(coords[1]);
 		request.setAttribute("s", vo);
 		
 		request.setAttribute("memberId", session.getAttribute("memberId"));
