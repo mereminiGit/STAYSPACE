4398342d307d38788a899a2da31c0c4c72583be8
jeongjaeryun <innisee@naver.com>
2023-10-06 09:38:22 +0900

    탈퇴수정

--- teamproject/src/main/java/co/yedam/teamproject/member/ajaxUserDelete.java
+++ teamproject/src/main/java/co/yedam/teamproject/member/ajaxUserDelete.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import co.yedam.teamproject.member.service.MemberService;
 import co.yedam.teamproject.member.service.MemberVO;
@@ -22,11 +23,13 @@ public class ajaxUserDelete extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String memberId=request.getParameter("memberId");
 		MemberService dao = new MemberServiceImpl();
+		HttpSession session = request.getSession();
 		
 		int result = dao.memberDelete(memberId);
 		
 		if(result !=0) {
 			response.getWriter().print("{\"retCode\": \"Success\"}");
+			session.invalidate();
 		}else {
 			response.getWriter().print("{\"retCode\": \"Fail\"}");
 		}
